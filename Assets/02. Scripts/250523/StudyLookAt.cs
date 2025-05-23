1d89b839f405b8d67f488278065d6e8b073a57ff
SuaKim <sbc605@naver.com>
2025-05-23 19:13:46 +0900

    [250523] 복습
    코드 재확인 및 저장

--- Assets/02. Scripts/250523/StudyLookAt.cs
+++ Assets/02. Scripts/250523/StudyLookAt.cs
@@ -5,6 +5,7 @@ public class StudyLookAt : MonoBehaviour
     public Transform targetTf; // 바라봐야하기 때문에 target이라는 변수 잡아야함
     public Transform turretHead;
 
+
     public GameObject bulletPrefab; // 총알 프리팹
     public Transform firePos; // 발사 위치
 
@@ -13,13 +14,17 @@ public class StudyLookAt : MonoBehaviour
 
 
     void Start() // 1번 실행 > 무엇인가를 셋팅하는 기능
+  
     {
+        GameObject target = GameObject.FindGameObjectWithTag("Player");
+
         turretHead = transform.GetChild(0);
-        targetTf = GameObject.FindGameObjectWithTag("Player").transform; // 플레이어태그 캐릭터를 찾아서 타겟에 넣음       
+        targetTf = target.transform; // 플레이어태그 캐릭터를 찾아서 타겟에 넣음       
     }
 
     void Update() // 무엇인가를 바라보는 기능
     {
+
        turretHead.LookAt(targetTf); // 터렛헤드가 타겟을 바라봄
 
         timer += Time.deltaTime;

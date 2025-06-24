3f166dd40a8104075035b0b48f0c5214d1102ce7
sbc605 <sbc605@naver.com>
2025-06-24 18:21:27 +0900

    [250624] 조이스틱 움직임 / Blend Tree / 점프 버튼 / 웅크리기 / 공격 버튼과 콤보

--- Assets/02. Scripts/7Week/250623/JoystickController.cs
+++ Assets/02. Scripts/7Week/250623/JoystickController.cs
@@ -1,19 +1,43 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class JoystickController : MonoBehaviour
+public class JoystickController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-    public void OnLog(string msg) // 기능체크용 로그
-    {
-        Debug.Log(msg);
-    }
+    [SerializeField] private KnightController_Joystick knightController;
+    [SerializeField] private GameObject backgroundUI;
+    [SerializeField] private GameObject handlerUI;
+
+    private Vector2 startPos, currPos;
 
     void Start()
     {
-        
+        backgroundUI.SetActive(false);
     }
 
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        backgroundUI.SetActive(true);
+        backgroundUI.transform.position = eventData.position;
+        startPos = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        currPos = eventData.position;
+        Vector2 dragDir = currPos - startPos; // 현재 드래그하는 방향
+
+        float maxDist = Mathf.Min(dragDir.magnitude, 75f); // a와 b중 최소값을 뽑는다.
+
+        handlerUI.transform.position = startPos + dragDir.normalized * maxDist;
+
+        knightController.InputJoystick(dragDir.x, dragDir.y);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        knightController.InputJoystick(0, 0);
+        handlerUI.transform.localPosition = Vector2.zero;
+        backgroundUI.SetActive(false);
     }
 }
--- Assets/02. Scripts/7Week/250623/KnightController_Joystick.cs
+++ Assets/02. Scripts/7Week/250623/KnightController_Joystick.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class KnightController_Joystick : MonoBehaviour
 {
     private Animator animator;
     private Rigidbody2D knightRb;
 
+    [SerializeField] private Button jumpButton;
+    [SerializeField] private Button attackButton;
+
     private Vector3 inputDir;
     [SerializeField] private float moveSpeed = 3f;
     [SerializeField] private float jumpPower = 30f;
-    private bool isGround = true;
+    private bool isGround, isCombo, isAttack;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         knightRb = GetComponent<Rigidbody2D>();
+
+        jumpButton.onClick.AddListener(Jump);
+        attackButton.onClick.AddListener(Attack);
     }
 
     void Update() // 일반적인 작업
@@ -44,37 +51,92 @@ public class KnightController_Joystick : MonoBehaviour
         }
     }
 
+    public void InputJoystick(float x, float y)
+    {
+        inputDir = new Vector3(x, y, 0).normalized;
+
+        animator.SetFloat("JoystickX", inputDir.x);
+        animator.SetFloat("JoystickY", inputDir.y);
+    }
 
 
     void Move()
     {
         if (inputDir.x != 0)
         {
+            var scaleX = inputDir.x > 0 ? 1 : -1;
+            transform.localScale = new Vector3(scaleX, 1, 1);
+
             knightRb.linearVelocityX = inputDir.x * moveSpeed;
         }
     }
 
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        if (isGround)
         {
             animator.SetTrigger("Jump");
             knightRb.AddForceY(jumpPower, ForceMode2D.Impulse);
         }
     }
 
-    void SetAnimation()
+    void Attack()
     {
-        if (inputDir.x != 0)
+        if (!isAttack) // 기본값이 false여서 이게 실행됨
         {
-            animator.SetBool("isRun", true);
-
-            var scaleX = inputDir.x > 0 ? 1 : -1;
-            transform.localScale = new Vector3(scaleX, 1, 1);
+            isAttack = true;
+            animator.SetTrigger("Attack");
         }
-        else if (inputDir.x == 0)
+        else
         {
-            animator.SetBool("isRun", false);
+            isCombo = true;
+        }
+        
+    }
+
+    public void CheckCombo()
+    {
+        if (isCombo)
+        {
+            animator.SetBool("isCombo", true);
+        }
+
+        else
+        {
+            animator.SetBool("isCombo", false);
+            isAttack = false;
         }
     }
+
+    public void EndCombo()
+    {
+        isAttack = false;
+        isCombo = false;
+    }
+
+    /* 요약 버전
+    void Attack()
+    {
+        if (!isAttack) // 기본값이 false여서 이게 실행됨
+        {
+            isAttack = true;
+            animator.SetTrigger("Attack");
+        }
+        else
+        {
+            animator.SetBool("isCombo", true);
+        }
+    }
+
+    public void CheckCombo()
+    {
+        if (!animator.GetBool("isCombo"))
+            isAttack = false;
+    }
+
+    public void EndCombo()
+    {
+        isAttack = false;
+        animator.SetBool("isCombo", false);
+    } */
 }
